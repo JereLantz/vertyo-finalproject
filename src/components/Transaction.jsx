5bc7588c2af3457c2f0b7ce99196b566f659e6d1
Jere Lantz <lantz.jere@gmail.com>
2025-04-13 09:16:55 +0300

    implemented displaying the saved transactions to the page

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,8 +1,8 @@
-export default function Transaction(){
+export default function Transaction({transaction}){
     return (
         <div className="flex m-2 p-3 border border-black">
-        <p className="m-2">Kuvaus</p>
-        <p className="m-2">Määrä</p>
+        <p className="m-2">{transaction.description}</p>
+        <p className="m-2">{transaction.amount}</p>
         <button className="py-2 px-1.5 mx-1 rounded-xl bg-red-300 hover:bg-red-400">Poista</button>
         </div>
     )
--- src/components/TransactionsDisplay.jsx
+++ src/components/TransactionsDisplay.jsx
@@ -1,12 +1,17 @@
 import Transaction from "./Transaction";
+import {TransactionContext} from "../context/TransactionsContext"
+import { use } from "react";
 
 export default function TransactionsDisplay(){
+    const {savedTransactions} = use(TransactionContext)
+
     return(
         <div>
             <h2 className="text-xl font-bold mx-2">Tapahtumat</h2>
+        {savedTransactions.length >0 && (
             <div>
-                <Transaction />
-            </div>
+            {savedTransactions.map((transa)=><Transaction key={transa.id} transaction={transa}/>)}
+            </div>)}
         </div>
     )
 }
