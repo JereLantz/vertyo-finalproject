ea4b4fe24d00830da0f3aaf641ebea6341173a04
Jere Lantz <lantz.jere@gmail.com>
2025-04-16 09:49:00 +0300

    now displaying just a button when no transactions are present, to prevent download csv component from erroring

--- src/components/DownloadDataCSV.jsx
+++ src/components/DownloadDataCSV.jsx
@@ -26,8 +26,16 @@ export default function Reactcsv(){
     const {savedTransactions} = use(TransactionContext)
 
     return(
+        <>
+        {savedTransactions.length == 0 ?(
+            <button className="m-2 rounded-xl p-2 bg-amber-100 hover:bg-amber-200">
+            Lataa tapahtumat
+            </button>
+        ):(
         <CSVLink data={savedTransactions} target="_blank" className="m-2 rounded-xl p-2 bg-amber-100 hover:bg-amber-200" filename="transactions.csv">
             Lataa tapahtumat
         </CSVLink>
+        )}
+        </>
     )
 }
