b6b1d25aa18fb1d8976f0ca8ab217f905fe620d7
Jere Lantz <lantz.jere@gmail.com>
2025-04-16 16:14:37 +0300

    the download component now also uses the filtered transaction list

--- src/components/DownloadDataCSV.jsx
+++ src/components/DownloadDataCSV.jsx
@@ -23,16 +23,16 @@ import { TransactionContext } from "../context/TransactionsContext"
 import { use } from "react"
 
 export default function Reactcsv(){
-    const {savedTransactions} = use(TransactionContext)
+    const {filteredTransactions} = use(TransactionContext)
 
     return(
         <>
-        {savedTransactions.length == 0 ?(
+        {filteredTransactions.length == 0 ?(
             <button className="m-2 rounded-xl p-2 bg-amber-100 hover:bg-amber-200">
             Lataa tapahtumat
             </button>
         ):(
-        <CSVLink data={savedTransactions} target="_blank" className="m-2 rounded-xl p-2 bg-amber-100 hover:bg-amber-200" filename="transactions.csv">
+        <CSVLink data={filteredTransactions} target="_blank" className="m-2 rounded-xl p-2 bg-amber-100 hover:bg-amber-200" filename="transactions.csv">
             Lataa tapahtumat
         </CSVLink>
         )}
