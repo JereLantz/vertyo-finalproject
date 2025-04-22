43100adcf41384e0921aef49dfa783fbc39075aa
Jere Lantz <lantz.jere@gmail.com>
2025-04-22 22:25:41 +0300

    added few more tests to the transaction component

--- src/components/Transaction.test.jsx
+++ src/components/Transaction.test.jsx
@@ -2,34 +2,26 @@ import { render, screen } from "@testing-library/react";
 import Transaction from "./Transaction"
 import userEvent from "@testing-library/user-event";
 import TransactionContextProvider, { TransactionContext } from "../context/TransactionsContext";
+import { exact } from "prop-types";
 
 describe("Individual transaction tests", ()=>{
         const transaction = {
             id:"1",
             description:"testi1",
-            amount:"10",
+            amount:10,
             category:"viihde",
         }
 
-    test("Renders the description of an item", ()=>{
+    test("Renders the item", ()=>{
         render(<Transaction transaction={transaction}/>)
 
-        const element = screen.getByText("testi1")
-        expect(element).toBeInTheDocument()
-    })
+        const elementDesc = screen.getByText("testi1")
+        const elementAmount = screen.getByText("10",{exact:false})
+        const elementCategory = screen.getByText("viihde",{exact:false})
 
-    test("Renders the amount of an item", ()=>{
-        render(<Transaction transaction={transaction}/>)
-
-        const element = screen.getByText("10",{exact:false})
-        expect(element).toBeInTheDocument()
-    })
-
-    test("Renders the category of an item", ()=>{
-        render(<Transaction transaction={transaction}/>)
-
-        const element = screen.getByText("viihde",{exact:false})
-        expect(element).toBeInTheDocument()
+        expect(elementDesc).toBeInTheDocument()
+        expect(elementAmount).toBeInTheDocument()
+        expect(elementCategory).toBeInTheDocument()
     })
 
     test("Switches to the modify form when 'muokkaa' button is pressed", async()=>{
@@ -57,18 +49,42 @@ describe("Individual transaction tests", ()=>{
         expect(textFields).toHaveLength(3)
     })
 
-    /*
-    test("After modifying, sending the network request", async()=>{
-        //TODO:
+    test("Modifying works", async()=>{
         const mockValue = {
-            updateTransaction: jest.fn(),
+            updateTransaction: vi.fn(()=>({success:true})),
         }
+        const transactionCopy = {...transaction}
 
         render(
             <TransactionContext value={mockValue}>
-                <Transaction transaction={transaction}/>
+                <Transaction transaction={transactionCopy}/>
             </TransactionContext >
         )
+
+        const modBtn = screen.getByText("Muokkaa")
+        await userEvent.click(modBtn)
+
+        const confirmBtn = screen.getByText("Vahvista")
+        await userEvent.click(confirmBtn)
+
+        expect(mockValue.updateTransaction).toHaveBeenCalledWith(transaction)
     })
-    */
+
+    test("Opens the delete confirmation with the transaction that will be deleted", async()=>{
+        const mockValue = {
+            showDeleteConfirm: vi.fn(),
+        }
+        const transactionCopy = {...transaction}
+
+        render(
+            <TransactionContext value={mockValue}>
+                <Transaction transaction={transactionCopy}/>
+            </TransactionContext >
+        )
+
+        const delButton = screen.getByText("Poista")
+        await userEvent.click(delButton)
+
+        expect(mockValue.showDeleteConfirm).toHaveBeenCalledWith(transaction)
+    })
 })
